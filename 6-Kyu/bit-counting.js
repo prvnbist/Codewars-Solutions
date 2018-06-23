3fba58a75c790e335a18a93301bcd500dde1b921
Praveen Bisht <prvnbist@gmail>
2018-06-23 16:18:26 +0530

    Refactored

--- 6-Kyu/bit-counting.js
+++ 6-Kyu/bit-counting.js
@@ -1,8 +1,14 @@
 //////////////////////////////////
 // Vanilla Js
 
+// Refactored
+var countBits = n => n.toString(2).replace(/[0]/g, "").length;
+
+// Old Code
 function countBits(n) {
-	var num = (n >>> 0).toString(2), count = 0;
-	for (var i=0;i<num.length;i++) if (num.charAt(i) === '1') count++;
+	var num = (n >>> 0).toString(2),
+		count = 0;
+	for (var i = 0; i < num.length; i++)
+		if (num.charAt(i) === '1') count++;
 	return count;
 };
